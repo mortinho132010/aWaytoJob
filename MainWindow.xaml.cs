e66d1719115724cf7fbdd600bbc1817232c78012
CLEVERSON SILVA <cleverson.silva@fatec.sp.gov.br>
2018-10-10 00:34:08 -0300

    el primero

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using awtj.Controles;
+namespace awtj {
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            UcLogin log = new UcLogin();
+            conteiner.Children.Add(log);
+        }
+    }
+}
